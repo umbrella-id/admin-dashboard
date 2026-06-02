ed303c865d080ee42af4218b1017904e12ab1520
umbrella-id <konemoba@gmail.com>
2026-06-02 10:20:54 +0700

    Delete admin-kas.js

--- admin-kas.js
+++
@@ -1,9 +0,0 @@
-// admin-kas.js - Minimal Version
-console.log("admin-kas.js loaded");
-
-function initKasDashboard(admin) {
-    console.log("initKasDashboard called", admin);
-    alert("KAS Dashboard: " + admin?.nama);
-}
-
-window.initKasDashboard = initKasDashboard;

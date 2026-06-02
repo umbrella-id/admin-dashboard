7779177198b5a5554d3de4c76e8e40385a177b50
umbrella-id <konemoba@gmail.com>
2026-06-02 11:55:07 +0700

    Update admin-kas.js

--- admin-kas.js
+++ admin-kas.js
@@ -1,6 +1,12 @@
 // admin-kas.js - Start from working version
 console.log("admin-kas.js loaded");
 
+function formatDate(date) {
+    if (!date) return "-";
+    const d = new Date(date);
+    return `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear().toString().slice(-2)} ${d.getHours().toString().padStart(2,'0')}:${d.getMinutes().toString().padStart(2,'0')}`;
+}
+
 function formatNumber(num) {
     if (num === undefined || num === null) return "0";
     return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");

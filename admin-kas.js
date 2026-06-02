4d2bf17deef17c57b8cdf5c4629c18b775dd5163
umbrella-id <konemoba@gmail.com>
2026-06-02 11:57:18 +0700

    Update admin-kas.js

--- admin-kas.js
+++ admin-kas.js
@@ -7,16 +7,10 @@ function formatDate(date) {
     return `${d.getDate()}/${d.getMonth()+1}/${d.getFullYear().toString().slice(-2)} ${d.getHours().toString().padStart(2,'0')}:${d.getMinutes().toString().padStart(2,'0')}`;
 }
 
-function formatNumber(num) {
-    if (num === undefined || num === null) return "0";
-    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-}
-
 function initKasDashboard(admin) {
     console.log("initKasDashboard called", admin);
     alert("KAS Dashboard: " + admin?.nama);
+    console.log("Test formatDate:", formatDate(new Date()));
 }
-
-// TEST: apakah alert masih muncul?
 
 window.initKasDashboard = initKasDashboard;

a0d1664768876df1278007347fe29dff91fab555
umbrella-id <konemoba@gmail.com>
2026-06-02 11:51:34 +0700

    Update admin-kas.js

--- admin-kas.js
+++ admin-kas.js
@@ -1,10 +1,12 @@
 // admin-kas.js - Start from working version
 console.log("admin-kas.js loaded");
 
-function initKasDashboard(admin) {
-    console.log("initKasDashboard called", admin);
+function formatNumber(num) {
+    if (num === undefined || num === null) return "0";
+    return num.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
     alert("KAS Dashboard: " + admin?.nama);
 }
+
 
 // TEST: apakah alert masih muncul?
 
